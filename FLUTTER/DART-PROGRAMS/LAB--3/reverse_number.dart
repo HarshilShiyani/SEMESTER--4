806b99f9b9226da44260b7f16c0fc165d59c9b4e
HarshilShiyani <95012680+HarshilShiyani@users.noreply.github.com>
2023-01-24 19:10:42 +0530

    Update reverse_number.dart

--- FLUTTER/DART-PROGRAMS/LAB--3/reverse_number.dart
+++ FLUTTER/DART-PROGRAMS/LAB--3/reverse_number.dart
@@ -4,7 +4,7 @@ void main(List<String> args) {
   stdout.write("Enter A Number : ");
   int num = int.parse(stdin.readLineSync()!);
   int rev = 0, rem;
-  while (num != 0) {
+  while (num > 0) {
     rem = num % 10;
     rev = rev * 10 + rem;
     num = (num ~/ 10);
